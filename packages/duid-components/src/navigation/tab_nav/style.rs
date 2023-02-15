3396fa422df590962b71e775bbeb80f15dd1cfd1
djedou <djedouarnaud.aman@gmail.com>
2023-02-15 22:23:12 +0100

    tabnav selectors

--- packages/duid-components/src/navigation/tab_nav/style.rs
+++ packages/duid-components/src/navigation/tab_nav/style.rs
@@ -6,7 +6,7 @@ pub(crate) fn get_tab_nav_selectors() -> HashSet<String> {
 
     let _ = selectors.insert(".tabnav:::mt-0 mb-4 border-b border-solid border-gray-200 ".to_owned());
     let _ = selectors.insert(".tabnav-tabs:::flex mb-1[-1px] overflow-auto".to_owned());
-    let _ = selectors.insert(".tabnav-tab:::inline-block shrink-0 py-2 px-4 text-xs color-gray-800 no-underline bg-color-transparent border border-solid border-transparent border-b-0 transition-none[color] duration-75[0.2s] ease-in[cubic-bezier(0.3,&0,&0.5,&1)]".to_owned());
+    let _ = selectors.insert(".tabnav-tab:::inline-block shrink-0 py-2 px-4 text-xs color-gray-800 no-underline bg-color-transparent rounded-tl-md rounded-tr-md border border-solid border-transparent border-b-0 transition-none[color] duration-75[0.2s] ease-in[cubic-bezier(0.3,&0,&0.5,&1)]".to_owned());
     let _ = selectors.insert(".tabnav-tab.selected, .tabnav-tab[aria-selected=true], .tabnav-tab[aria-current]:not([aria-current=false]):::color-gray-800 bg-color-gray-200 border-gray-400 border-b-0 rounded-tl-md rounded-tr-md rounded-br-none rounded-bl-none".to_owned());
     let _ = selectors.insert(".tabnav-tab.selected .octicon, .tabnav-tab[aria-selected=true] .octicon, .tabnav-tab[aria-current]:not([aria-current=false]) .octicon:::color-inherit".to_owned());
     let _ = selectors.insert(".tabnav-tab:hover:::color-gray-800 no-underline duration-75[0.1s]".to_owned());
