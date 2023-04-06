682800e5a02200f76df0ccc8c5bf93fd345438f2
djedou <djedouarnaud.aman@gmail.com>
2023-04-06 14:19:23 +0200

    review select menu style

--- duid-components/src/inputs/select_menu/styles.rs
+++ duid-components/src/inputs/select_menu/styles.rs
@@ -4,11 +4,11 @@ use std::collections::HashSet;
 pub(crate) fn default_select_menu_selectors() -> HashSet<String> {
     let mut selectors = HashSet::with_capacity(0);
     
-    selectors.insert(".details-reset[open] > summary::before:::fixed inset-0 z-4 block cursor-inherit content-[\"&\"] bg-color-transparent".to_owned());
+    selectors.insert(".details-reset[open] > summary::before:::fixed inset-0 z-4 block cursor-default content-[\"&\"] bg-color-transparent".to_owned());
     selectors.insert(".details-reset > summary::before:::hidden".to_owned());
     selectors.insert(".select-menu-container:::relative inline-block".to_owned());
     selectors.insert(".select-menu-container-right-aligned:::float-right".to_owned());
-    selectors.insert(".SelectMenu-modal:::absolute max-h-max min-w-max my-1 mx-0 pointer-events-auto".to_owned()); //  animation[SelectMenu-modal-animation&0.12s&cubic-bezier(0,&0.1,&0.1,&1)&backwards;@keyframes&SelectMenu-modal-animation&{0%&{opacity:&0;transform:&scale(0.9);}}]
+    selectors.insert(".SelectMenu-modal:::absolute z-8 max-h-max min-w-max my-1 mx-0 pointer-events-auto".to_owned()); //  animation[SelectMenu-modal-animation&0.12s&cubic-bezier(0,&0.1,&0.1,&1)&backwards;@keyframes&SelectMenu-modal-animation&{0%&{opacity:&0;transform:&scale(0.9);}}]
     //selectors.insert(".SelectMenu-modal:::absolute max-h-max min-w-max z-4 my-1 mx-0 overflow-hidden  bg-color-gray-50 border border-solid border-slate-400 rounded-md shadow-md shadow-slate-400".to_owned()); //  animation[SelectMenu-modal-animation&0.12s&cubic-bezier(0,&0.1,&0.1,&1)&backwards;@keyframes&SelectMenu-modal-animation&{0%&{opacity:&0;transform:&scale(0.9);}}]
     selectors.insert(".SelectMenu-modal-right-aligned:::right-0".to_owned());
     //selectors.insert(".SelectMenu-list:::p-0 mt-0 mr-0 ml-0 mb-0[-1px] bg-color-gray-50".to_owned());
